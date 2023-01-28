90465c6a0aa96e95e3f47e3199ea08cc4c4c0019
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-01-28 15:38:44 -0800

    caches movies fetched

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,6 +6,13 @@ import type { MovieDbResponse } from '$lib/types';
 type OutputType = { popular_movies: Promise<MovieDbResponse> };
 
 // 👇 since this is only called on the server, we can bypass HTTP 💡
-export const load: PageServerLoad<OutputType> = async (event) => ({
-	popular_movies: router.createCaller(await createContext(event)).popular_movies()
-});
+export const load: PageServerLoad<OutputType> = async (event) => {
+	// cache page data for an hour
+	event.setHeaders({
+		'cache-control': 'public, max-age=3600'
+	});
+
+	return {
+		popular_movies: router.createCaller(await createContext(event)).popular_movies()
+	};
+};
