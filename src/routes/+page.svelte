335dd27fc74b2a2396902d168d1ada6e6b6ff44a
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-01-29 10:57:37 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 
 <div class="bg-white">
 	<div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-		<h2 class="sr-only">Products</h2>
+		<h2 class="sr-only">Movies</h2>
 
 		<div
 			class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8"
