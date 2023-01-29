3f3064819395692854a72035172fc0fa0cdb834e
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-01-28 16:12:12 -0800

    added movie_genres

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -13,6 +13,19 @@ export const router = t.router({
 			`${MOVIEDB_API_URL}/movie/popular?api_key=${MOVIEDB_API_KEY}&language=en-US&page=1`
 		).then((res) => res.json());
 		return response;
+	}),
+	movie_genres: t.procedure.query(async () => {
+		const response: { genres: { id: number; name: string }[] } = await fetch(
+			`${MOVIEDB_API_URL}/genre/movie/list?api_key=${MOVIEDB_API_KEY}&language=en-US`
+		).then((res) => res.json());
+
+		const genres: { [key: number]: string } = {};
+
+		response.genres.map((genre) => {
+			genres[genre.id] = genre.name;
+		});
+
+		return genres;
 	})
 });
 
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,10 @@ import { router } from '$lib/trpc/router';
 import type { PageServerLoad } from './$types';
 import type { MovieDbResponse } from '$lib/types';
 
-type OutputType = { popular_movies: Promise<MovieDbResponse> };
+type OutputType = {
+	popular_movies: Promise<MovieDbResponse>;
+	movie_genres: Promise<{ [key: number]: string }>;
+};
 
 // 👇 since this is only called on the server, we can bypass HTTP 💡
 export const load: PageServerLoad<OutputType> = async (event) => {
@@ -13,6 +16,7 @@ export const load: PageServerLoad<OutputType> = async (event) => {
 	});
 
 	return {
-		popular_movies: router.createCaller(await createContext(event)).popular_movies()
+		popular_movies: router.createCaller(await createContext(event)).popular_movies(),
+		movie_genres: router.createCaller(await createContext(event)).movie_genres()
 	};
 };
