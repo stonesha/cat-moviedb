5b33f7c9427138ddcb3262c0fd115d82fac3fc61
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-01-29 10:52:28 -0800

    initial navbar done

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,138 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { createDisclosure } from 'svelte-headlessui';
+
+	const mobile_menu = createDisclosure({ label: 'Mobile Menu', expanded: false });
+</script>
+
+<nav class="bg-gray-800">
+	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+		<div class="relative flex h-16 items-center justify-between">
+			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+				<!-- Mobile menu button-->
+				<button
+					type="button"
+					class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+					aria-controls="mobile-menu"
+					aria-expanded="false"
+					use:mobile_menu.button
+				>
+					<span class="sr-only">Open main menu</span>
+					{#if $mobile_menu.expanded}
+						<svg
+							class="h-6 w-6"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					{:else}
+						<svg
+							class="block h-6 w-6"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+							/>
+						</svg>
+					{/if}
+				</button>
+			</div>
+			<div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+				<a class="flex flex-shrink-0 items-center text-white font-bold" href="/">cat-moviedb</a>
+				<div class="hidden sm:ml-6 sm:block">
+					<div class="flex space-x-4">
+						<a
+							href="/"
+							class={`${
+								$page.route.id === '/' ? 'bg-gray-900' : 'hover:bg-gray-700 hover:text-white'
+							} text-white px-3 py-2 rounded-md text-sm font-medium`}
+							aria-current="page">Home</a
+						>
+
+						<a
+							href="/movies"
+							class={`${
+								$page.route.id?.includes('/movies')
+									? 'bg-gray-900'
+									: 'hover:bg-gray-700 hover:text-white'
+							} text-white px-3 py-2 rounded-md text-sm font-medium`}>Movies</a
+						>
+
+						<a
+							href="/tv-shows"
+							class={`${
+								$page.route.id?.includes('/tv-shows')
+									? 'bg-gray-900'
+									: 'hover:bg-gray-700 hover:text-white'
+							} text-white px-3 py-2 rounded-md text-sm font-medium`}>TV Shows</a
+						>
+
+						<a
+							href="/people"
+							class={`${
+								$page.route.id?.includes('/people')
+									? 'bg-gray-900'
+									: 'hover:bg-gray-700 hover:text-white'
+							} text-white px-3 py-2 rounded-md text-sm font-medium`}>People</a
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	{#if $mobile_menu.expanded}
+		<div class="sm:hidden" id="mobile-menu" use:mobile_menu.panel>
+			<div class="space-y-1 px-2 pt-2 pb-3">
+				<a
+					href="/"
+					class={`${
+						$page.route.id === '/'
+							? 'bg-gray-900 text-white'
+							: 'hover:bg-gray-700 hover:text-white text-gray-300'
+					} block px-3 py-2 rounded-md text-base font-medium`}
+					aria-current="page">Home</a
+				>
+
+				<a
+					href="/movies"
+					class={`${
+						$page.route.id?.includes('/movies')
+							? 'bg-gray-900 text-white'
+							: 'hover:bg-gray-700 hover:text-white text-gray-300'
+					} block px-3 py-2 rounded-md text-base font-medium`}>Movies</a
+				>
+
+				<a
+					href="/tv-shows"
+					class={`${
+						$page.route.id?.includes('/tv-shows')
+							? 'bg-gray-900 text-white'
+							: 'hover:bg-gray-700 hover:text-white text-gray-300'
+					} block px-3 py-2 rounded-md text-base font-medium`}>TV Shows</a
+				>
+
+				<a
+					href="/people"
+					class={`${
+						$page.route.id?.includes('/people')
+							? 'bg-gray-900 text-white'
+							: 'hover:bg-gray-700 hover:text-white text-gray-300'
+					} block px-3 py-2 rounded-md text-base font-medium`}>People</a
+				>
+			</div>
+		</div>
+	{/if}
+</nav>
