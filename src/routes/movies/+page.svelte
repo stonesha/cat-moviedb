a43e3157025a42ce66c0cb1b1cd8553a27b53497
Stone Sha <36431006+stonesha@users.noreply.github.com>
2023-02-17 00:10:53 -0800

    changed slug

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -38,7 +38,7 @@
 				</div>
 				<div class="flex flex-1 flex-col space-y-2 p-4">
 					<h3 class="text-md font-medium text-gray-900">
-						<a href={`/movie/${movie.id}`}>
+						<a href={`/movies/${movie.id}`}>
 							<span aria-hidden="true" class="absolute inset-0" />
 							{movie.original_title}
 						</a>
